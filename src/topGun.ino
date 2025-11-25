9d4d3edc66dfadcd9868fd415fdf6289983b6b43
Fabian Calangiu <fabian.calangiu@studio.unibo.it>
2025-11-25 17:25:26 +0100

    Set up .ino

--- src/topGun.ino
+++ src/topGun.ino
@@ -1,7 +1,29 @@
+#include "Arduino.h"
+#include "Timer.h"
+#include "servo_motor_impl.h"
+#include "servo_motor.h"
+
+#define PIR 10
+#define TRIG 13
+#define ECHO 12
+#define LED 7
+#define BTN 6
+#define SERVO 5
+
+int pos;   
+int delta;
+ServoMotor* pMotor;
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  pinMode(BTN, INPUT);
+  pinMode(LED, OUTPUT);
+
+  pMotor = new ServoMotorImpl(SERVO);
+  pos = 0;
+  delta = 1;
 }
 
 void loop() {
-  Serial.println("BUON LAVORO");
+  
 }
